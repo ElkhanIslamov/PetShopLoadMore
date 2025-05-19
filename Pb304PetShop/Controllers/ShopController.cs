c5ee31a90a2e7f6a5bbc8084094f38d66b2ac46f
Elxan Islamov <elkhan.islamov@acsc.az>
2025-05-19 16:00:00 +0400

    LoadMore

---
+++ Pb304PetShop/Controllers/ShopController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Pb304PetShop.DataContext;
+
+namespace Pb304PetShop.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ShopController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.ProductCount = await _context.Products.CountAsync();   
+            var products =await _context.Products.Take(6).ToListAsync();
+            return View(products);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Partial([FromBody] RequestModel requsetModel)
+        {
+            var products = await _context.Products.Skip(requsetModel.StartFrom).Take(6).ToListAsync();
+            return PartialView("_ProductPartialView",products);
+        }
+    }
+    public class  RequestModel
+    {
+        public int StartFrom { get; set; }
+     }
+}
